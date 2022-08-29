55c7ca88673998040063b7db86617d3e34f043b2
Amit32624 <120220153@umail.ucc.ie>
2022-08-29 14:15:27 +0100

    new file

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    console.log("Amit")
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
